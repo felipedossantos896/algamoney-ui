2f9930fc9bbd195a625af614310346ff42759b63
Felipe S. Carneiro <felipedossantos896@gmail.com>
2019-06-17 09:57:53 -0300

    Adicionando o componente pessoas-pesquisa

---
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent {
+
+  constructor() { }
+
+}
